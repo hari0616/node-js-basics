827eb0440b14edaf93e2341f102d4248ec8e518d
Haritha <s.haridha@claritytts.com>
2022-08-11 15:07:59 +0530

    changes

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.send('welcome node-js-basics'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
